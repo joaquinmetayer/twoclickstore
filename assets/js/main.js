a291d2c26ff42b706ec8279e90914066eb203a3b
Joaquin Metayer <joaquinmetayer@gmail.com>
2022-10-06 20:02:57 -0300

    294

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,9 +20,17 @@ let linkIphone = document.getElementsByClassName('link-iphone')
 let linkMacbook = document.getElementsByClassName('link-macbook')
 let linkAirpods = document.getElementsByClassName('link-airpods')
 
+// navbar cart item counter
+let navItemCounter = document.getElementById('btn-carrito-counter')
+itemsCount = 0
 
+// total cart
+let cartTotal = document.getElementById('total-card')
 
-
+// carrito
+let emptyItem = document.getElementById('empty-item')
+let cartContainer = document.getElementById('items-cart')
+let totalCart = 0
 
 // listen events
 function startPage(){
@@ -33,9 +41,9 @@ function startPage(){
     allItem.style.display = 'flex'
     console.log("startPage")
 }
+
 // functions
-
-
+// aplicar filtros
 function makeFilter(str) {
     console.log("inMakeFilter")
     if(str == "all"){
@@ -83,6 +91,20 @@ function makeFilter(str) {
         console.log('airpodsFilterAplied')
     }
 }
+// agregar a carrito
+function addToCart(str){
+    // quito empty item ya que el carro no esta mas vacio
+    emptyItem.style.display = 'none';
+    //sumo contador
+    itemsCount++
+    navItemCounter.innerHTML = itemsCount
+
+    // si es igual a iphone1 y no existe agregar este
+    // si el contador es mayor a uno solo sumar contador
+    cartContainer.innerHTML += '<div class="d-flex align-items-center justify-content-between p-2"><span class="fw-bold">iPhone 14 Pro Max - 1TB</span><div><span class="me-2">$ 1,599.0</span><button type="button" class="btn btn-danger" onclick="">Eliminar</button></div></div>'
+    
+}
+
 
 // main functions
 
