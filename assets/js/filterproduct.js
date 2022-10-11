4333fd57b3bcbcc330a32cd6acc582c031325f3f
Joaquin Metayer <joaquinmetayer@gmail.com>
2022-10-11 12:29:37 -0300

    346

--- assets/js/filterproduct.js
+++ assets/js/filterproduct.js
@@ -18,11 +18,13 @@ function filterProducts(str){
     itemsIphone = document.getElementsByClassName('iphone')[0]
     itemsIpad = document.getElementsByClassName('ipad')[0]
     itemsMacbook = document.getElementsByClassName('macbook')[0]
+
     // genero otro con misma clase para iterar
     allsAirpods = document.getElementsByClassName('airpods')
     allsIphone = document.getElementsByClassName('iphone')
     allsIpad = document.getElementsByClassName('ipad')
     allsMacbook = document.getElementsByClassName('macbook')
+
     // si coincide muestro y oculto
     switch(str){
         case 'airpods':
@@ -30,30 +32,55 @@ function filterProducts(str){
             for (const itemsIpad of allsIpad){itemsIpad.style.display = 'none'}
             for (const itemsIphone of allsIphone){itemsIphone.style.display = 'none'}
             for (const itemsAirpods of allsAirpods){itemsAirpods.style.display = 'flex'}
+            btnAirpods.classList.add('underline')
+            btnIphone.classList.remove('underline')
+            btnIpad.classList.remove('underline')
+            btnMacbook.classList.remove('underline')
+            btnTodos.classList.remove('underline')
             break
         case 'iphone':
             for (const itemsMacbook of allsMacbook){itemsMacbook.style.display = 'none'}
             for (const itemsIpad of allsIpad){itemsIpad.style.display = 'none'}
             for (const itemsIphone of allsIphone){itemsIphone.style.display = 'flex'}
             for (const itemsAirpods of allsAirpods){itemsAirpods.style.display = 'none'}
+            btnIphone.classList.add('underline')
+            btnAirpods.classList.remove('underline')
+            btnIpad.classList.remove('underline')
+            btnMacbook.classList.remove('underline')
+            btnTodos.classList.remove('underline')
             break
         case 'ipad':
             for (const itemsMacbook of allsMacbook){itemsMacbook.style.display = 'none'}
             for (const itemsIpad of allsIpad){itemsIpad.style.display = 'flex'}
             for (const itemsIphone of allsIphone){itemsIphone.style.display = 'none'}
             for (const itemsAirpods of allsAirpods){itemsAirpods.style.display = 'none'}
+            btnIpad.classList.add('underline')
+            btnIphone.classList.remove('underline')
+            btnAirpods.classList.remove('underline')
+            btnMacbook.classList.remove('underline')
+            btnTodos.classList.remove('underline')
             break
         case 'macbook':
             for (const itemsMacbook of allsMacbook){itemsMacbook.style.display = 'flex'}
             for (const itemsIpad of allsIpad){itemsIpad.style.display = 'none'}
             for (const itemsIphone of allsIphone){itemsIphone.style.display = 'none'}
             for (const itemsAirpods of allsAirpods){itemsAirpods.style.display = 'none'}
+            btnMacbook.classList.add('underline')
+            btnIphone.classList.remove('underline')
+            btnIpad.classList.remove('underline')
+            btnAirpods.classList.remove('underline')
+            btnTodos.classList.remove('underline')
             break
         case 'todos':
             for (const itemsMacbook of allsMacbook){itemsMacbook.style.display = 'flex'}
             for (const itemsIpad of allsIpad){itemsIpad.style.display = 'flex'}
             for (const itemsIphone of allsIphone){itemsIphone.style.display = 'flex'}
             for (const itemsAirpods of allsAirpods){itemsAirpods.style.display = 'flex'}
+            btnTodos.classList.add('underline')
+            btnIphone.classList.remove('underline')
+            btnIpad.classList.remove('underline')
+            btnMacbook.classList.remove('underline')
+            btnAirpods.classList.remove('underline')
             break
     }
 }
